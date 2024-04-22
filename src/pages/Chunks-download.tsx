a30e29bae407342f8a8720fac3732dc483aa7dd2
Chavan Pranit <chavapra@ap.dtcs.local>
2024-04-22 13:25:04 +0530

    Changed processing warning text, if user refreshes on download page they will be redirected to home page

--- src/pages/Chunks-download.tsx
+++ src/pages/Chunks-download.tsx
@@ -1,9 +1,18 @@
 import VideoPreview from '@/components/video-preview';
 import Container from '@/components/container';
 import { useVideoStorageState } from '@/stores/video';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function ChunksDownload() {
   const chunkUrls = useVideoStorageState((state) => state.chunkUrls);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (chunkUrls.length === 0) {
+      navigate('/');
+    }
+  }, []);
 
   return (
     <>
