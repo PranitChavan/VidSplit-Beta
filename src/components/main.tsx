f5b8f811b7cb426fc91d8b242449f06aab87abec
Chavan Pranit <chavapra@ap.dtcs.local>
2024-05-04 11:38:08 +0530

    Replaced 'Split' with 'Cut' on homepage and app description

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -12,7 +12,7 @@ export default function Main() {
     <>
       <header>
         <h2 className="scroll-m-20 text-2xl sm:text-3xl font-semibold tracking-tight text-center sm:p-3 px-5 pt-7">Free video splitter for stories</h2>
-        <p className="text-xl text-muted-foreground text-center p-3">Split lengthy videos into into stories! Best for Instagram, Whatsapp, Facebook, Snapchat & more. No watermark!</p>
+        <p className="text-xl text-muted-foreground text-center p-3">Cut lengthy videos into into stories! Best for Instagram, Whatsapp, Facebook, Snapchat & more. No watermark!</p>
       </header>
       <main>
         <Container>
